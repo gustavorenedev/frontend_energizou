ece19c56e31a4066d27f2ae4d5215e582309ec84
Gustavo René <renegustavo42@gmail.com>
2023-11-03 20:30:45 -0300

    feat: Add to table in home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,32 @@
-import Footer from "../../components/footer/footer";
-import Header from "../../components/header/header";
+import { useEffect, useState } from "react";
+import Footer from "../../components/footer/Footer";
+import Header from "../../components/header/Header";
+import Table from "../../components/table/Table";
 
 export default function Home() {
+  const [companyList, setCompanyList] = useState([]);
+
+  const getCompanies = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/companies");
+      const data = await response.json();
+      setCompanyList(data);
+    } catch (error) {
+      alert(`Error: ${error}`);
+    }
+  };
+
+  useEffect(() => {
+    getCompanies();
+  }, []);
+
   return (
     <div>
       <Header />
+      <section className="contentContainer">
+        <h2>Tabela de empresas</h2>
+        <Table list={companyList} />
+      </section>
       <Footer />
     </div>
   );
