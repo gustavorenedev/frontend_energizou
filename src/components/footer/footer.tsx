c4196a377f83673aeffb90088114ece29247eecc
Gustavo René <renegustavo42@gmail.com>
2023-11-03 18:32:02 -0300

    feat: Finish Footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,5 +1,26 @@
+import logoLinkedin from "../../assets/linkedinIcon.png";
+import logoGithub from "../../assets/githubIcon.png";
+import "./Footer.style.less";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="elementFooter">
+      <div className="footerContainer">
+        <p>Desenvolvido por: Gustavo René</p>
+        <div className="footerBox">
+          <a
+            href="https://www.linkedin.com/in/gustavo-rene-dias/"
+            target="_blank"
+          >
+            <img src={logoLinkedin} alt="Logo linkedin" />
+          </a>
+          <a href="https://github.com/gustavorenedev" target="_blank">
+            <img src={logoGithub} alt="Logo github" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
