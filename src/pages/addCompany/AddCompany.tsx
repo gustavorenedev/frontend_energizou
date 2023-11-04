242ec27adf8c4ed5d37d4b278901628893050391
Gustavo René <renegustavo42@gmail.com>
2023-11-04 14:39:11 -0300

    feat: Implemented validation for cnpj exist, possible errors and style for errors

--- src/pages/addCompany/AddCompany.tsx
+++ src/pages/addCompany/AddCompany.tsx
@@ -2,17 +2,9 @@ import React, { useState } from "react";
 import CompanyForm from "../../components/form/Form";
 import Layout from "../../components/layout/Layout";
 import { Button } from "../../components/button/Button";
-import {
-  isValidClientName,
-  isValidPassword,
-  isValidCompanyName,
-  isValidCnpj,
-  isValidCep,
-  isValidAddress,
-  isValidNumber,
-  isValidPhone,
-  isValidEmail,
-} from "../../utils/validations";
+import { validateForm } from "../../utils/validationsErrors";
+import ErrorMessage from "../../components/errorMessage/errorMessage";
+import { checkExistingCnpj } from "../../api/service/checkCnpj";
 import { useNavigate } from "react-router-dom";
 import { routes } from "../../routes";
 import "./AddCompany.style.less";
@@ -32,47 +24,67 @@ const AddCompany = () => {
     company_email: "",
   });
 
+  const [formErrors, setFormErrors] = useState({
+    client_name: null,
+    client_password: null,
+    company_name: null,
+    company_cnpj: null,
+    company_zip_code: null,
+    company_address: null,
+    company_number: null,
+    company_phone: null,
+    company_email: null,
+  });
+
+  const [cnpjError, setCnpjError] = useState<string | null>(null);
+
   const handleBack = () => {
     navigate(routes.home);
   };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
+
+    if (name === "company_cnpj") {
+      setCnpjError(null);
+    }
     setFormData({
       ...formData,
       [name]: value,
     });
   };
 
-  const handleCreate = () => {
-    if (
-      isValidClientName(formData.client_name) &&
-      isValidPassword(formData.client_password) &&
-      isValidCompanyName(formData.company_name) &&
-      isValidCnpj(formData.company_cnpj) &&
-      isValidCep(formData.company_zip_code) &&
-      isValidAddress(formData.company_address) &&
-      isValidNumber(formData.company_number) &&
-      isValidPhone(formData.company_phone) &&
-      isValidEmail(formData.company_email)
-    ) {
-      fetch("http://localhost:3000/companies", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      })
-        .then((response) => {
-          if (response.ok) {
-            console.log("Solicitação POST bem-sucedida");
-          } else {
-            console.error("Erro na solicitação POST");
-          }
+  const handleSubmit = async () => {
+    const errors = validateForm(formData);
+
+    setFormErrors(errors);
+
+    if (Object.values(errors).every((error) => error === null)) {
+
+      const cnpjExists = await checkExistingCnpj(formData.company_cnpj);
+
+      if (cnpjExists) {
+        setCnpjError("CNPJ já existe, por favor, escolha outro.");
+      } else {
+
+        fetch("http://localhost:3000/companies", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
         })
-        .catch((error) => {
-          console.error("Erro na solicitação POST", error);
-        });
+          .then((response) => {
+            if (response.ok) {
+              console.log("Solicitação POST bem-sucedida");
+            } else {
+              console.error("Erro na solicitação POST");
+            }
+          })
+          .catch((error) => {
+            console.error("Erro na solicitação POST", error);
+          });
+      }
     }
   };
 
@@ -81,9 +93,19 @@ const AddCompany = () => {
       <div>
         <h1>Insira todos os dados</h1>
         <CompanyForm formData={formData} onChange={handleChange} />
+        <ErrorMessage error={formErrors.client_name} />
+        <ErrorMessage error={formErrors.client_password} />
+        <ErrorMessage error={formErrors.company_name} />
+        <ErrorMessage error={formErrors.company_cnpj} />
+        <ErrorMessage error={formErrors.company_zip_code} />
+        <ErrorMessage error={formErrors.company_address} />
+        <ErrorMessage error={formErrors.company_number} />
+        <ErrorMessage error={formErrors.company_phone} />
+        <ErrorMessage error={formErrors.company_email} />
+        <ErrorMessage error={cnpjError} />
         <div className="boxButtons">
           <Button onClick={handleBack}>Voltar</Button>
-          <Button onClick={handleCreate}>Criar Empresa</Button>
+          <Button onClick={handleSubmit}>Criar Empresa</Button>
         </div>
       </div>
     </Layout>
