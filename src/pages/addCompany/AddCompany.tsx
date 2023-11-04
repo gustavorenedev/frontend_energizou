efacfe29025c09cc7f6ba001e417b697b2bf3aa2
Gustavo René <renegustavo42@gmail.com>
2023-11-04 00:28:04 -0300

    feat: Add form validations and styling

--- src/pages/addCompany/AddCompany.tsx
+++ src/pages/addCompany/AddCompany.tsx
@@ -2,8 +2,24 @@ import React, { useState } from "react";
 import CompanyForm from "../../components/form/Form";
 import Layout from "../../components/layout/Layout";
 import { Button } from "../../components/button/Button";
+import {
+  isValidClientName,
+  isValidPassword,
+  isValidCompanyName,
+  isValidCnpj,
+  isValidCep,
+  isValidAddress,
+  isValidNumber,
+  isValidPhone,
+  isValidEmail,
+} from "../../utils/validations";
+import { useNavigate } from "react-router-dom";
+import { routes } from "../../routes";
+import "./AddCompany.style.less";
 
 const AddCompany = () => {
+  const navigate = useNavigate();
+
   const [formData, setFormData] = useState({
     client_name: "",
     client_password: "",
@@ -16,6 +32,10 @@ const AddCompany = () => {
     company_email: "",
   });
 
+  const handleBack = () => {
+    navigate(routes.home);
+  };
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setFormData({
@@ -25,25 +45,35 @@ const AddCompany = () => {
   };
 
   const handleCreate = () => {
-    console.log("Dados do formulário:", formData);
-
-    fetch("http://localhost:3000/companies", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    })
-      .then((response) => {
-        if (response.ok) {
-          console.log("Solicitação POST bem-sucedida");
-        } else {
-          console.error("Erro na solicitação POST");
-        }
+    if (
+      isValidClientName(formData.client_name) &&
+      isValidPassword(formData.client_password) &&
+      isValidCompanyName(formData.company_name) &&
+      isValidCnpj(formData.company_cnpj) &&
+      isValidCep(formData.company_zip_code) &&
+      isValidAddress(formData.company_address) &&
+      isValidNumber(formData.company_number) &&
+      isValidPhone(formData.company_phone) &&
+      isValidEmail(formData.company_email)
+    ) {
+      fetch("http://localhost:3000/companies", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
       })
-      .catch((error) => {
-        console.error("Erro na solicitação POST", error);
-      });
+        .then((response) => {
+          if (response.ok) {
+            console.log("Solicitação POST bem-sucedida");
+          } else {
+            console.error("Erro na solicitação POST");
+          }
+        })
+        .catch((error) => {
+          console.error("Erro na solicitação POST", error);
+        });
+    }
   };
 
   return (
@@ -51,7 +81,10 @@ const AddCompany = () => {
       <div>
         <h1>Insira todos os dados</h1>
         <CompanyForm formData={formData} onChange={handleChange} />
-        <Button onClick={handleCreate}>Criar Empresa</Button>
+        <div className="boxButtons">
+          <Button onClick={handleBack}>Voltar</Button>
+          <Button onClick={handleCreate}>Criar Empresa</Button>
+        </div>
       </div>
     </Layout>
   );
---
+++ src/utils/validations.ts
@@ -0,0 +1,68 @@
+// utils.ts
+export const isValidClientName = (name: string): string | null => {
+  if (name.trim() === "") {
+    return "Nome do Cliente é obrigatório.";
+  }
+  return null;
+};
+
+export const isValidPassword = (password: string): string | null => {
+  if (password.length < 6) {
+    return "A senha deve ter pelo menos 6 caracteres.";
+  }
+  return null;
+};
+
+export const isValidCompanyName = (name: string): string | null => {
+  if (name.trim() === "") {
+    return "Nome da empresa (Razão Social) é obrigatório.";
+  }
+  return null;
+};
+
+export const isValidCnpj = (cnpj: string): string | null => {
+  const cnpjPattern = /^[0-9]{2}\.[0-9]{3}\.[0-9]{3}\/[0-9]{4}-[0-9]{2}$/;
+  if (!cnpjPattern.test(cnpj)) {
+    return "CNPJ inválido. Deve seguir o formato XX.XXX.XXX/XXXX-XX";
+  }
+  return null;
+};
+
+export const isValidCep = (cep: string): string | null => {
+  const cepPattern = /^[0-9]{5}-[0-9]{3}$/;
+  if (!cepPattern.test(cep)) {
+    return "CEP inválido. Deve seguir o formato XXXXX-XXX";
+  }
+  return null;
+};
+
+export const isValidAddress = (address: string): string | null => {
+  if (address.trim() === "") {
+    return "Endereço é obrigatório.";
+  }
+  return null;
+};
+
+export const isValidNumber = (number: string): string | null => {
+  const numberPattern = /^[1-9][0-9]{0,3}$/;
+  if (!numberPattern.test(number)) {
+    return "Número inválido. Deve ser um número inteiro válido.";
+  }
+  return null;
+};
+
+export const isValidPhone = (phone: string): string | null => {
+  const phonePattern = /^\+55 \([0-9]{2}\) [0-9]{5}-[0-9]{4}$/;
+  if (!phonePattern.test(phone)) {
+    return "Telefone inválido. Deve seguir o formato +55(XX)XXXXX-XXXX";
+  }
+  return null;
+};
+
+export const isValidEmail = (email: string): string | null => {
+  const emailPattern = /^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$/;
+  if (!emailPattern.test(email)) {
+    return "Email inválido. Deve ser um endereço de email válido.";
+  }
+  return null;
+};
