b76ee5209b06e7e833a898ab89b93f254cf1168f
Gustavo René <renegustavo42@gmail.com>
2023-11-05 14:39:10 -0300

    refactor: refactoring the POST route

--- src/pages/addCompany/AddCompany.tsx
+++ src/pages/addCompany/AddCompany.tsx
@@ -4,15 +4,15 @@ import Layout from "../../components/layout/Layout";
 import { Button } from "../../components/button/Button";
 import { validateForm } from "../../utils/validationsErrors";
 import ErrorMessage from "../../components/errorMessage/errorMessage";
-import { checkExistingCnpj } from "../../api/service/checkCnpj";
 import { useNavigate } from "react-router-dom";
 import { routes } from "../../routes";
 import "./AddCompany.style.less";
+import { createCompany, checkExistingCnpj } from "../../api/service/fetchApis";
 
 const AddCompany = () => {
   const navigate = useNavigate();
 
-  const [formData, setFormData] = useState({
+  const initialFormData = {
     client_name: "",
     client_password: "",
     company_name: "",
@@ -22,22 +22,13 @@ const AddCompany = () => {
     company_number: "",
     company_phone: "",
     company_email: "",
-  });
+  };
 
+  const [formData, setFormData] = useState(initialFormData);
   const [formErrors, setFormErrors] = useState({
-    client_name: null,
-    client_password: null,
-    company_name: null,
-    company_cnpj: null,
-    company_zip_code: null,
-    company_address: null,
-    company_number: null,
-    company_phone: null,
-    company_email: null,
+    ...initialFormData,
   });
-
   const [cnpjError, setCnpjError] = useState<string | null>(null);
-
   const [successMessage, setSuccessMessage] = useState<string | null>(null);
 
   const handleBack = () => {
@@ -50,6 +41,7 @@ const AddCompany = () => {
     if (name === "company_cnpj") {
       setCnpjError(null);
     }
+
     setFormData({
       ...formData,
       [name]: value,
@@ -62,42 +54,37 @@ const AddCompany = () => {
     setFormErrors(errors);
 
     if (Object.values(errors).every((error) => error === null)) {
-      const cnpjExists = await checkExistingCnpj(formData.company_cnpj);
+      try {
+        const cnpjExists = await checkExistingCnpj(formData.company_cnpj);
 
-      if (cnpjExists) {
-        setCnpjError("CNPJ já existe, por favor, escolha outro.");
-      } else {
-        try {
-          const response = await fetch("http://localhost:3000/companies", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(formData),
-          });
-
-          if (response.ok) {
-            setSuccessMessage("Empresa cadastrada com sucesso.");
-            setFormData({
-              // Limpe os campos após o sucesso
-              client_name: "",
-              client_password: "",
-              company_name: "",
-              company_cnpj: "",
-              company_zip_code: "",
-              company_address: "",
-              company_number: "",
-              company_phone: "",
-              company_email: "",
-            });
-          } else {
-            console.error("Erro na solicitação POST");
-          }
-        } catch (error) {
-          console.error("Erro na solicitação POST", error);
+        if (cnpjExists) {
+          setCnpjError("CNPJ já existe, por favor, escolha outro.");
+        } else {
+          const successMessage = await createCompany(formData);
+          setSuccessMessage(successMessage);
         }
+      } catch (error) {
+        console.error(error);
       }
     }
+  };
+
+  const renderErrorMessages = () => {
+    const errorFields = [
+      "client_name",
+      "client_password",
+      "company_name",
+      "company_cnpj",
+      "company_zip_code",
+      "company_address",
+      "company_number",
+      "company_phone",
+      "company_email",
+    ];
+
+    return errorFields.map((field) => (
+      <ErrorMessage key={field} error={formErrors[field]} />
+    ));
   };
 
   return (
@@ -105,15 +92,7 @@ const AddCompany = () => {
       <div>
         <h1>Insira todos os dados</h1>
         <CompanyForm formData={formData} onChange={handleChange} />
-        <ErrorMessage error={formErrors.client_name} />
-        <ErrorMessage error={formErrors.client_password} />
-        <ErrorMessage error={formErrors.company_name} />
-        <ErrorMessage error={formErrors.company_cnpj} />
-        <ErrorMessage error={formErrors.company_zip_code} />
-        <ErrorMessage error={formErrors.company_address} />
-        <ErrorMessage error={formErrors.company_number} />
-        <ErrorMessage error={formErrors.company_phone} />
-        <ErrorMessage error={formErrors.company_email} />
+        {renderErrorMessages()}
         <ErrorMessage error={cnpjError} />
         {successMessage && <p>{successMessage}</p>}
         <div className="boxButtons">
